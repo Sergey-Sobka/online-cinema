feb56111090910b669b4bf139f36d964c68aacd1
Reverad <sektumpro@gmail.com>
2026-07-06 18:47:48 +0300

    add cart schemas

---
+++ app/schemas/cart.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, ConfigDict
+
+from app.models import Movie
+
+
+class CartItemRead(BaseModel):
+    id: int
+    cart_id: int
+    movie: Movie
+    
+    model_config = ConfigDict(from_attributes=True)
+
+
+class CartRead(BaseModel):
+    id: int
+    user_id: int
+    cart_items: list[CartItemRead]
+
+    model_config = ConfigDict(from_attributes=True)
