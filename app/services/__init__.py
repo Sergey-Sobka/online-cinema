b8dab68e4afd30283ca87eba192c7508358760f2
Reverad <sektumpro@gmail.com>
2026-07-06 18:49:03 +0300

    add services for cart

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,1 +1,6 @@
 """Service layer modules."""
+from app.services.cart import add_movie_to_cart
+
+__all__ = [
+    "add_movie_to_cart",
+]
---
+++ app/services/cart.py
@@ -0,0 +1,30 @@
+from fastapi import HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app import crud
+
+
+async def add_movie_to_cart(db: AsyncSession, user_id: int, movie_id: int):
+    cart = await crud.cart.get_cart_by_user_id(db, user_id=user_id)
+
+    if not cart:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Cart is not found"
+        )
+
+    for item in cart.cart_items:
+        if item.movie_id == movie_id:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Movie is already in your cart.",
+            )
+
+    is_purchased = False  # Placeholder
+
+    if is_purchased:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="You already purchased this movie.",
+        )
+
+    return await crud.cart.add_item_to_cart(db, cart_id=cart.id, movie_id=movie_id)
