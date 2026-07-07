2e13e20563de05b7d34f5ca252b6d8ed276e06c3
Reverad <sektumpro@gmail.com>
2026-07-07 14:51:16 +0300

    update services/cart.py

--- app/services/cart.py
+++ app/services/cart.py
@@ -1,8 +1,21 @@
 from fastapi import HTTPException, status
+from sqlalchemy import exists, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app import crud
-from app.models import CartItem
+from app.models import CartItem, Order, OrderItem, OrderStatus
+
+
+async def is_movie_purchased(db: AsyncSession, user_id: int, movie_id: int) -> bool:
+    stmt = (
+        select(exists())
+        .where(Order.user_id == user_id)
+        .where(Order.status == OrderStatus.PAID)
+        .where(OrderItem.order_id == Order.id)
+        .where(OrderItem.movie_id == movie_id)
+    )
+    result = await db.execute(stmt)
+    return result.scalar_one()
 
 
 async def add_movie_to_cart(db: AsyncSession, user_id: int, movie_id: int) -> CartItem:
@@ -20,7 +33,11 @@ async def add_movie_to_cart(db: AsyncSession, user_id: int, movie_id: int) -> Ca
                 detail="Movie is already in your cart.",
             )
 
-    is_purchased = False  # Placeholder
+    is_purchased = await is_movie_purchased(
+        db,
+        user_id=user_id,
+        movie_id=movie_id
+    )
 
     if is_purchased:
         raise HTTPException(
