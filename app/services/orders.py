1f394da353f4ea6bd1ceca27f9b9c3117f8387ab
pawunder1992 <pavelkryvutsa@gmail.com>
2026-07-07 13:29:49 +0300

    MYPY

--- app/services/orders.py
+++ app/services/orders.py
@@ -17,7 +17,7 @@ class OrderService:
         self._session = session
 
     async def place_order(self, current_user: User, movie_ids: list[int]) -> Order:
-        movie_ids = set(movie_ids)
+        movie_ids = list(dict.fromkeys(movie_ids))
         movies = (
             await self._session.scalars(select(Movie).where(Movie.id.in_(movie_ids)))
         ).all()
