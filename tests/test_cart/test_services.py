bdedd83f7e20aa4b592c14dba88c3716879725c6
Reverad <sektumpro@gmail.com>
2026-07-07 14:58:20 +0300

    update test_services.py

--- tests/test_cart/test_services.py
+++ tests/test_cart/test_services.py
@@ -13,23 +13,6 @@ class DummyMovie:
 
 @pytest.mark.asyncio
 class TestAddMovieToCartService:
-    async def test_add_movie_to_cart_success(self, db_session: AsyncSession):
-        user_id = 11
-        movie_id = 42
-
-        cart = Cart(user_id=user_id)
-        db_session.add(cart)
-        await db_session.commit()
-        await db_session.refresh(cart)
-
-        cart_item = await add_movie_to_cart(
-            db=db_session, user_id=user_id, movie_id=movie_id
-        )
-
-        assert cart_item is not None
-        assert cart_item.cart_id == cart.id
-        assert cart_item.movie_id == movie_id
-
     async def test_add_movie_to_cart_not_found(self, db_session: AsyncSession):
         non_existent_user_id = 999
         movie_id = 42
