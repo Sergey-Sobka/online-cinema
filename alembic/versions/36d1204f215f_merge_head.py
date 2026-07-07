5220edb4fa72dac2fe66ca9ce227148d059a0bdf
pawunder1992 <pavelkryvutsa@gmail.com>
2026-07-07 13:27:40 +0300

    1

--- alembic/versions/36d1204f215f_merge_head.py
+++ alembic/versions/36d1204f215f_merge_head.py
@@ -5,14 +5,11 @@ Revises: 202607061000, b8c9e20fef27
 Create Date: 2026-07-07 13:16:28.847273
 
 """
+
 from collections.abc import Sequence
 
-from alembic import op
-import sqlalchemy as sa
-
-
-revision: str = '36d1204f215f'
-down_revision: str | None = ('202607061000', 'b8c9e20fef27')
+revision: str = "36d1204f215f"
+down_revision: str | None = ("202607061000", "b8c9e20fef27")
 branch_labels: str | Sequence[str] | None = None
 depends_on: str | Sequence[str] | None = None
 
