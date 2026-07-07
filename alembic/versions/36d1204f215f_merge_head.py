defda06e5426686128e6947d9d277a5086fa03cf
pawunder1992 <pavelkryvutsa@gmail.com>
2026-07-07 13:21:50 +0300

    last migration

---
+++ alembic/versions/36d1204f215f_merge_head.py
@@ -0,0 +1,25 @@
+"""merge head
+
+Revision ID: 36d1204f215f
+Revises: 202607061000, b8c9e20fef27
+Create Date: 2026-07-07 13:16:28.847273
+
+"""
+from collections.abc import Sequence
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = '36d1204f215f'
+down_revision: str | None = ('202607061000', 'b8c9e20fef27')
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
