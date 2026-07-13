cb137db70df39d9b8dd82e2931b69745f7b0bd45
pawunder1992 <pavelkryvutsa@gmail.com>
2026-07-13 22:47:24 +0300

    added user repository

---
+++ app/repositories/users.py
@@ -0,0 +1,12 @@
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import User
+
+
+class UserRepository:
+    def __init__(self, session: AsyncSession) -> None:
+        self._session = session
+
+    async def get_user_by_id(self, user_id: int) -> User:
+        return await self._session.scalar(select(User).where(User.id == user_id))
