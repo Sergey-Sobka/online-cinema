8b152ea1329b62a226db058137997cd5ff918c76
pawunder1992 <pavelkryvutsa@gmail.com>
2026-07-24 22:18:42 +0300

    Add PurchasedMovieService to manage retrieval of purchased movies with permission checks

---
+++ app/services/purchased_movies.py
@@ -0,0 +1,21 @@
+from fastapi import HTTPException
+
+from app.core.uow_abstraction import IUnitOfWork
+from app.models import User, UserGroupEnum
+from app.models.purchased_movies import PurchasedMovie
+
+
+class PurchasedMovieService:
+    def __init__(self, uow: IUnitOfWork):
+        self.uow = uow
+
+    async def get_purchased_movies(self, current_user: User, user_id: int) -> list[PurchasedMovie]:
+        async with self.uow:
+            if current_user.id != user_id and current_user.group.name not in (UserGroupEnum.ADMIN,
+                                                                             UserGroupEnum.MODERATOR):
+                raise HTTPException(
+                    status_code=403,
+                    detail="You do not have permission to view other users' purchased movies."
+                )
+            else:
+                return await self.uow.purchased_movies.get_purchased_movies(user_id)
