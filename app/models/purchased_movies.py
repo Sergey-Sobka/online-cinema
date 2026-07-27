d41609b7c26d3f7aacd729d2e6f1976569b3ddb6
pawunder1992 <pavelkryvutsa@gmail.com>
2026-07-27 19:35:35 +0300

    Refactor PurchasedMovie model for improved readability and consistency

--- app/models/purchased_movies.py
+++ app/models/purchased_movies.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import ForeignKey, DateTime, func, UniqueConstraint
+from sqlalchemy import DateTime, ForeignKey, UniqueConstraint, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.db.base import Base
@@ -9,13 +9,15 @@ from app.db.base import Base
 class PurchasedMovie(Base):
     __tablename__ = "purchased_movies"
 
-    id : Mapped[int] = mapped_column(primary_key=True)
-    user_id : Mapped[int] = mapped_column(ForeignKey("users.id"), ondelete="CASCADE")
-    movie_id : Mapped[int] = mapped_column(ForeignKey("movies.id"), ondelete="CASCADE")
-    purchase_at : Mapped[datetime] = mapped_column(DateTime(timezone=True),
-                                                   server_default=func.now(),
-                                                   nullable=False)
-    movie = relationship("Movie")
-    __table_args__ = (
-        UniqueConstraint("user_id", "movie_id", name="uq_user_movie"),
+    id: Mapped[int] = mapped_column(primary_key=True)
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"), nullable=False
     )
+    movie_id: Mapped[int] = mapped_column(
+        ForeignKey("movies.id", ondelete="CASCADE"), nullable=False
+    )
+    purchase_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
+    movie = relationship("Movie")
+    __table_args__ = (UniqueConstraint("user_id", "movie_id", name="uq_user_movie"),)
